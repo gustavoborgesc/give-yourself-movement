676e975dfff754b6ed72c0eb3d7641afa6cc1c17
Gustavo Borges Costa <gustavob@kyros.com.br>
2021-02-28 17:50:40 -0300

    Desenvolvimento da aula05

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState, ReactNode, useEffect } from "react";
-
+import Cookies from "js-cookie";
 import challenges from "../../challenges.json";
+import LevelUpModal from "../components/LevelUpModal";
 
 interface Challenge {
     type: "body" | "eye";
@@ -15,6 +16,7 @@ interface ChallengesContextData {
     activeChallenge: Challenge;
     experienceToNextLevel: number;
     levelUp: () => void;
+    closeLevelUpModal: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
     completeChallenge: () => void;
@@ -22,15 +24,26 @@ interface ChallengesContextData {
 
 interface ChallengesProviderProps {
     children: ReactNode;
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({
+    children,
+    ...rest
+}: ChallengesProviderProps) {
+    const [level, setLevel] = useState(rest.level ?? 1);
+    const [currentExperience, setCurrentExperience] = useState(
+        rest.currentExperience ?? 0
+    );
+    const [challengesCompleted, setChallengesCompleted] = useState(
+        rest.challengesCompleted ?? 0
+    );
     const [activeChallenge, setActiveChallenge] = useState(null);
+    const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
@@ -38,8 +51,19 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         Notification.requestPermission();
     }, []);
 
+    useEffect(() => {
+        Cookies.set("level", String(level));
+        Cookies.set("currentExperience", String(currentExperience));
+        Cookies.set("challengesCompleted", String(challengesCompleted));
+    }, [level, currentExperience, challengesCompleted]);
+
     function levelUp() {
         setLevel(level + 1);
+        setIsLevelUpModalOpen(true);
+    }
+
+    function closeLevelUpModal() {
+        setIsLevelUpModalOpen(false);
     }
 
     function startNewChallenge() {
@@ -49,7 +73,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         const challenge = challenges[randomChallengesIndex];
         setActiveChallenge(challenge);
 
-        new Audio('/notification.mp3').play();
+        new Audio("/notification.mp3").play();
 
         if (Notification.permission === "granted") {
             new Notification("Novo desafio :)", {
@@ -89,12 +113,14 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 activeChallenge,
                 experienceToNextLevel,
                 levelUp,
+                closeLevelUpModal,
                 startNewChallenge,
                 resetChallenge,
                 completeChallenge,
             }}
         >
             {children}
+            {isLevelUpModalOpen && <LevelUpModal />}
         </ChallengesContext.Provider>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,68 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import ChallengeBox from "../components/ChallengeBox";
 import CompleteChallenges from "../components/CompleteChallenges";
 import Countdown from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 import { CountdownProvider } from "../contexts/CountdownContext";
 
 import styles from "../styles/pages/Home.module.css";
 
-export default function Home() {
+interface HomeProps {
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
+}
+
+export default function Home({
+    level,
+    currentExperience,
+    challengesCompleted,
+}: HomeProps) {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>Inicio | G.YS.M</title>
-            </Head>
+        <ChallengesProvider
+            level={level}
+            currentExperience={currentExperience}
+            challengesCompleted={challengesCompleted}
+        >
+            <div className={styles.container}>
+                <Head>
+                    <title>Inicio | G.YS.M</title>
+                </Head>
 
-            <ExperienceBar />
+                <ExperienceBar />
 
-            <CountdownProvider>
-                <section>
-                    <div>
-                        <Profile />
-                        <CompleteChallenges />
-                        <Countdown />
-                    </div>
-                    <div>
-                        <ChallengeBox />
-                    </div>
-                </section>
-            </CountdownProvider>
-        </div>
+                <CountdownProvider>
+                    <section>
+                        <div>
+                            <Profile />
+                            <CompleteChallenges />
+                            <Countdown />
+                        </div>
+                        <div>
+                            <ChallengeBox />
+                        </div>
+                    </section>
+                </CountdownProvider>
+            </div>
+        </ChallengesProvider>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const {
+        level = "1",
+        currentExperience = "0",
+        challengesCompleted = "0",
+    } = context.req.cookies;
+
+    return {
+        props: {
+            level: Number(level),
+            currentExperience: Number(currentExperience),
+            challengesCompleted: Number(challengesCompleted),
+        },
+    };
+};
